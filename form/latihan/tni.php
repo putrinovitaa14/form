2becd93ea3f8fa0db33456ccb21272debe73cfd3
putrinovitaa14 <putrinovitasari01411@gmail.com>
2021-12-01 15:47:31 +0700

    form latihan

---
+++ form/latihan/tni.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Pendaftaran TNI/Polri</legend>
+        <form action="tnipro.php" method="post">
+            <input type="text" name="nama" placeholder="Masukan Nama"><br>
+            <input type="radio" name="jk" value="Perempuan">Perempuan
+            <input type="radio" name="jk" value="Laki-laki">Laki-laki <br>
+            <input type="number" name="tb" placeholder="Masukan Tinggi Badan"><br>
+            <input type="number" name="bb" placeholder="Masukan Berat Badan"><br>
+            <button name="save">simpan</button>
+        
+        </form>
+    </fieldset>
+    
+</body>
+</html>
---
+++ form/latihan/tnipro.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['save'])) {
+    $nama = $_POST['nama'];
+    $jk = $_POST['jk'];
+    $tb = $_POST['tb'];
+    $bb = $_POST['bb'];
+
+    if (($jk == "Laki-laki" && $tb > 170 && $bb < 90) || ($jk == "Perempuan" && $tb > 165 && $bb < 75)) {
+        $ket = "Diterima";
+    } else {
+        $ket = "Tidak Diterima";
+    }
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Nama : <b><?php echo $nama; ?></b> <br>
+    Jenis Kelamin : <b><?php echo $jk; ?></b> <br>
+    Tinggi Badan : <b><?php echo $tb; ?></b> <br>
+    Berat Badan : <b><?php echo $bb; ?></b> <br>
+    Keterangan : <b><?php echo $ket; ?></b>
+
+</body>
+</html>
+<?php }?>
